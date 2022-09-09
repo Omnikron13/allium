a4a991fc4cd11dd837862eac8933f4b2e723e6d9
Joey Sabey <joey.sabey@gmx.com>
2022-09-09 10:53:11 +0100

    skeleton

---
+++ run.py
@@ -0,0 +1,8 @@
+# Copyright 2022, Joey Sabey
+# 
+# Copying and distribution of this file, with or without modification, is permitted
+# in any medium without royalty, provided this copyright notice is preserved.
+# 
+# This file is offered as-is, without any warranty.
+
+
